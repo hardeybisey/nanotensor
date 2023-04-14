f5b46d5495ccfe79978bcead817159d6d60f428c
Idris Adebisi <104902482+hardeybisey@users.noreply.github.com>
2023-04-14 14:58:15 +0000

    adding files to the repo

---
+++ engine/tensor.py
@@ -0,0 +1,168 @@
+import numpy as np
+
+class Tensor:
+    """
+    A class representing a tensor with a single numerical value.
+
+    Parameters:
+    -----------
+    data : int or float
+        The numerical value of the tensor.
+    _downstream : tuple of Tensors, optional
+        Tensors that depend on this tensor. Defaults to an empty tuple.
+    _op : str, optional
+        The operation performed on this tensor. Defaults to an empty string.
+    dtype : type, optional
+        The data type of the tensor. Defaults to None.
+
+    Attributes:
+    -----------
+    data : float
+        The numerical value of the tensor.
+    grad : float
+        The gradient of the tensor with respect to the loss.
+    _downstream : tuple of Tensors
+        Tensors that depend on this tensor.
+    _op : str
+        The operation performed on this tensor.
+    """
+    def __init__(self, data, _downstream = tuple(), _op='', dtype=None):
+        """
+        Initialize a Tensor object.
+        Parameters:
+            data (int or float): The numerical value of the tensor.
+            _downstream (tuple, optional): A tuple of downstream tensors.
+            _op (str, optional): The operation that produced this tensor.
+            dtype (type, optional): The desired data type of the tensor.
+        Raises:
+            AssertionError: If `data` is not of type `int` or `float`.
+        """
+        assert isinstance(data , (int,float)), "data must be of type array, int or float"
+        self.data = float(data) if not dtype else dtype(data)
+        self._downstream = _downstream
+        self._backward = lambda: None
+        self._op = _op
+        self.grad = 0.0
+
+    def __add__(self, other):
+        """
+        Return the sum of this tensor and another tensor or numerical value.
+        Parameters:
+            other (Tensor or int or float): The other tensor or numerical value to add.
+        Returns:
+            Tensor: A new tensor representing the sum of this tensor and `other`.
+        """
+       other = other if isinstance(other, Tensor) else Tensor(other)
+       out =  Tensor(data =(self.data + other.data), _downstream=(self,other), _op= '+')
+       def _backward():
+          self.grad += 1.0 * out.grad
+          other.grad += 1.0 * out.grad
+       out._backward = _backward
+       return out  
+
+    def __mul__(self, other):
+        """
+        Return the product of this tensor and another tensor or numerical value.
+        Parameters:
+            other (Tensor or int or float): The other tensor or numerical value to multiply.
+
+        Returns:
+            Tensor: A new tensor representing the product of this tensor and `other`.
+        """
+       other = other if isinstance(other, Tensor) else Tensor(other)
+       out =  Tensor(data =(self.data * other.data), _downstream=(self,other), _op= '*') 
+       def _backward():
+         self.grad += other.data * out.grad
+         other.grad += self.data * out.grad
+       out._backward = _backward
+       return out 
+    
+    def __pow__(self, other):
+        """
+        Return this tensor raised to the power of another numerical value.
+
+        Parameters:
+            other (int or float): The numerical value to raise this tensor to the power of.
+
+        Returns:
+            Tensor: A new tensor representing this tensor raised to the power of `other`.
+        """
+       other = other if isinstance(other, Tensor) else Tensor(other)
+       out = Tensor(data = (self.data**other.data), _downstream=(self,), _op='pow')
+       def _backward():
+         self.grad += (other.data) * (self.data ** (other.data -1)) * out.grad
+         other.grad += (self.data) * (other.data ** (self.data -1)) * out.grad
+       out._backward = _backward
+       return out
+    
+    def relu(self):
+        out = Tensor(data=(0 if self.data < 0 else self.data), _downstream=(self,), _op='relu')
+        def _backward():
+            self.grad += out.data * out.grad
+        out._backward = _backward
+        return out
+    
+    def tanh(self):
+        t = (np.exp(2*self.data) - 1) / (np.exp(2*self.data) + 1)
+        out = Tensor(data = t, _downstream=(self, ),  _op='tanh')
+        def _backward():
+            self.grad += (1 - out.data**2) * out.grad
+        out._backward = _backward
+        return out
+       
+    def sigmoid(self):
+        s = 1 / (1 + np.exp(-self.data))
+        out = Tensor(data= s, _downstream=(self, ),  _op='sigmoid')
+        def _backward():
+            self.grad = out.data * (1- out.data)
+        out._backward = _backward
+        return out
+    
+    def exp(self):
+        x = np.exp(self.data)
+        out = Tensor(data=x, _downstream=(self, ), _op='exp')
+        def _backward():
+            self.grad += out.data * out.grad
+        out._backward = _backward        
+        return out   
+       
+    def backward(self):
+       nodes = self.__allnodes()
+       self.grad = 1
+       for node in nodes:
+         node._backward()
+    
+    def __allnodes(self):
+       nodes = []
+       def get_downstream(object, visited = set()):
+         if object not in visited:
+            visited.add(object)
+            nodes.append(object)
+            for children in object._downstream:
+               get_downstream(children)
+       get_downstream(self)
+       return nodes 
+    
+    def __neg__(self): 
+       return self * -1
+
+    def __radd__(self, other):
+       return self + other
+    
+    def __truediv__(self, other):
+       return self * (other**-1)
+    
+    def __rmul__(self,other):
+       return self * other
+    
+    def __sub__(self, other):
+      return self + (-other)
+    
+    def __rtruediv__(self, other):
+       return self * (other**-1)
+    
+    def __repr__(self):
+        """
+        Return a string representation of the Tensor object.
+        """
+      return f"Tensor(data= str({self.data}), grad=str({self.grad}))"
---
+++ module/layers.py
@@ -0,0 +1,85 @@
+from engine.tensor import Tensor
+
+class ZeroGrad:
+    def zero_grad(self):
+        for p in self.parameters():
+            p.grad = 0.0
+
+
+class Neuron(ZeroGrad):
+    def __init__(self, inx, activation):
+        """"
+        inx: number of input to the neuron
+        activation : None [relu, tanh, sigmoid]
+        """
+        self.weight = [Tensor(np.random.normal()) for _ in range(inx)]
+        self.bias = Tensor(np.random.normal())
+        if activation:
+            self.activation = activation.lower()
+    
+    def __call__(self, x):
+        out = sum((x*w for x,w in zip(x,self.weight))) + self.bias
+        if self.activation:
+            if self.activation == 'relu':
+                out = out.relu()
+            elif self.activation == 'tanh':
+                out = out.tanh()
+            elif self.activation == 'sigmoid':
+                out = out.sigmoid()
+        return out
+
+    def parameters(self):
+        parameters = self.weight + [self.bias]
+        return parameters 
+
+class Layer(ZeroGrad):
+    layerdict = {'relu': 'ReLU', 'tanh': 'Tanh', 'sigmoid': 'Sigmoid'}
+    def __init__(self, size=tuple(), activation=None):
+        """"
+        inx: number of input to the neuron
+        out: number of neurons in the layer
+        activation : None [relu, tanh, sigmoid]
+        """
+        self.activation = activation
+        self.shape = size
+        self.neurons = [Neuron(size[0],activation=activation) for _ in range(size[1])]       
+        self.totalparam = len(self.parameters())
+        
+    def __call__(self, x):
+        out = [n(x) for n in self.neurons]
+        return out[0] if len(out) < 2 else out
+
+    def parameters(self):
+        param = [p for neuron in self.neurons for p in neuron.parameters()]
+        return param
+
+    def __repr__(self):
+        return f"{'Linear' if not self.activation else Layer.layerdict[self.activation.lower()]}Layer       Shape: {self.shape})      Params: {self.totalparam}"
+        
+
+class Sequential(ZeroGrad):
+    def __init__(self, layers):
+        self.layers = layers
+        self.totalparams = len(self.parameters())
+
+    def __call__(self, x):
+        for layer in self.layers:
+            x = layer(x)
+        return x
+    
+    def parameters(self):
+        param = [p for layer in self.layers for p in layer.parameters()]
+        return param 
+    
+    def summary(self):
+        summary = self.__repr__()
+        print(summary) 
+
+    def __repr__(self):
+        layout = "Layer(type)         Shape              Parameters"
+        formatter = "\n---------------------------------------------------\n"
+        bottom = f"Trainable Parameters                          {self.totalparams}"
+        for layer in self.layers:
+            layout += formatter + str(layer)
+        summary = layout + formatter + bottom
+        return summary
